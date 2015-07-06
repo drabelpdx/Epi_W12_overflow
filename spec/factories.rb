d2a2e7580b1782d4a5429496dd7f0388c1c23182
cleri philiastides & ian bevel <student@epicodus.com>
2015-07-06 12:14:44 -0700

    add specs to test create user and log out functionality, pass both specs

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,4 +3,9 @@ FactoryGirl.define do
     email('ian@ian.com')
     password('password')
   end
+
+  factory(:question) do
+    question('What is 1 + 1 equal to?')
+    body("I've been at this for days. Please help.")
+  end
 end
