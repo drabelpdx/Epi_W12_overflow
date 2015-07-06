e62b17e07d18e5e3d7d820a75adf0fc6a43470e6
cleri philiastides & ian bevel <student@epicodus.com>
2015-07-06 12:09:33 -0700

    add specs to test create user and log out functionality, pass both specs

--- spec/features/log_out_page_spec.rb
+++ spec/features/log_out_page_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
-describe "the user log in process" do
-  it "logs in a user" do
+describe "the log out process" do
+  it "logs the user out" do
     user = FactoryGirl.create(:user)
     visit '/'
     click_on 'Log In'
     fill_in 'Email', :with => 'ian@ian.com'
     fill_in 'Password', :with => 'password'
     click_on 'Log in'
-    expect(page).to have_content "You've been logged in"
+    visit '/'
+    click_on 'Log Out'
+    expect(page).to have_content "You've been logged out successfully."
   end
 end
